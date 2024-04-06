79901c01a6988bd02aef3e0a26686c38442f5e3c
Khai <125385990+Khai0000@users.noreply.github.com>
2024-04-06 08:27:43 +0000

    Added header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/images/AnimalCENTRE.svg";
+import '../styles/Header.css';
 
 export const Header = () => {
   return (
     <header>
-        <h1>This is header</h1>
+      <div class="container">
+        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+          <a
+            href="/"
+            class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+          >
+            <img src={logo} class="bi me-2" width="70" height="60"  />
+            <span class="fs-4" style={{ fontWeight: "bold" }}>
+              AnimalCentre
+            </span>
+          </a>
+
+          <ul class="nav nav-pills">
+            <li class="nav-item">
+              <a href="#" class="nav-link active" aria-current="page">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+                Features
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+                Pricing
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+                FAQs
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+                About
+              </a>
+            </li>
+          </ul>
+        </header>
+      </div>
     </header>
-  )
-}
+  );
+};
